38829d6cb9d72693b2a2fe9c6e7625f720a870b8
namb1910261 <namb1910261@student.ctu.edu.vn>
2023-02-15 20:22:35 +0700

    cap nhat tim kiem dich vu

--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -8,6 +8,16 @@
         @csrf
         <input type="text" name="search" class="form-control border-0 shadow-none" placeholder="Search..." aria-label="Search..." />
     </form>
+@elseif(Request::is('dichvus') || Request::is('dichvus*'))
+    <form action="dichvus-search" method="GET">
+        @csrf
+        <input type="text" name="search" class="form-control border-0 shadow-none" placeholder="Search..." aria-label="Search..." />
+    </form>
+@elseif(Request::is('phongs') || Request::is('phongs*'))
+    <form action="phongs-search" method="GET">
+        @csrf
+        <input type="text" name="search" class="form-control border-0 shadow-none" placeholder="Search..." aria-label="Search..." />
+    </form>
 @elseif(Request::is('khachhangs') || Request::is('khachhangs*'))
     <form action="khachhangs-search" method="GET">
         @csrf
