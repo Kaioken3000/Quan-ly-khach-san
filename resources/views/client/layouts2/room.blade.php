52f003830ca8afcb637cd78a5080c8e26e27c951
Nhut Nam <nhutnam393@gmail.com>
2023-07-24 21:20:33 +0700

    sua loi

--- resources/views/client/layouts2/room.blade.php
+++ resources/views/client/layouts2/room.blade.php
@@ -5,59 +5,65 @@
             <div class="row list-wrapper">
                 {{-- Roon load begin --}}
                 @foreach ($phongs as $phong)
-                <?php $datphongs = App\Models\Datphong::get();
-                $check = 0;
-                $ngayvaodadat = "";
-                $ngayradadat = "";
-                foreach($datphongs as $datphong){
-                    $danhsachdatphong = App\Models\Danhsachdatphong::where("datphongid", $datphong->id)->latest()->first();
-                    if($danhsachdatphong->phongid == $phong->so_phong){
-                        $check++;
-                        $ngayvaodadat = $danhsachdatphong->ngaybatdauo;
-                        $ngayradadat = $danhsachdatphong->ngayketthuco;
-                        break;
+                    <?php $datphongs = App\Models\Datphong::get();
+                    $check = 0;
+                    $ngayvaodadat = '';
+                    $ngayradadat = '';
+                    $today = date('Y-m-d');
+                    foreach ($datphongs as $datphong) {
+                        $danhsachdatphong = App\Models\Danhsachdatphong::where('datphongid', $datphong->id)
+                            ->latest()
+                            ->first();
+                        if ($danhsachdatphong->phongid == $phong->so_phong && $datphong->huydatphong == 0) {
+                            if ($danhsachdatphong->ngayketthuco > $today) {
+                                $check++;
+                                $ngayvaodadat = $danhsachdatphong->ngaybatdauo;
+                                $ngayradadat = $danhsachdatphong->ngayketthuco;
+                                break;
+                            }
+                        }
                     }
-                }
-                ?>
-                <div class="col-lg-3 col-md-6 list-item">
-                    <div class="hp-room-item set-bg" data-setbg="/client/images/{{$phong->picture_1}}">
-                        <div class="hr-text">
-                            <h3>{{ $phong->loaiphongs->ten }} - {{ $phong->so_phong }}</h3>
-                            <h2>{{ $phong->loaiphongs->gia }}VND<span>/Pernight</span></h2>
-                            @if($check==0)
-                            <a href="/client/chitietphong/{{$phong->so_phong}}" class="primary-btn">More Details</a>
-                            <br>
-                            @else
-                            <div class="d-flex">
-                                <p class="mb-0 text-light"> Room is booked: &nbsp;</p>
-                                <div class="me-0">
-                                    <p class="mb-0 text-light"> since: {{$ngayvaodadat}}</p>
-                                    <p class="mb-0 text-light"> to: {{$ngayradadat}}</p>
-                                </div>
+                    ?>
+                    <div class="col-lg-3 col-md-6 list-item">
+                        <div class="hp-room-item set-bg" data-setbg="/client/images/{{ $phong->picture_1 }}">
+                            <div class="hr-text">
+                                <h3>{{ $phong->loaiphongs->ten }} - {{ $phong->so_phong }}</h3>
+                                <h2>{{ $phong->loaiphongs->gia }}VND<span>/Pernight</span></h2>
+                                @if ($check == 0)
+                                    <a href="/client/chitietphong/{{ $phong->so_phong }}" class="primary-btn">More
+                                        Details</a>
+                                    <br>
+                                @else
+                                    <div class="d-flex">
+                                        <p class="mb-0 text-light"> Room is booked: &nbsp;</p>
+                                        <div class="me-0">
+                                            <p class="mb-0 text-light"> since: {{ $ngayvaodadat }}</p>
+                                            <p class="mb-0 text-light"> to: {{ $ngayradadat }}</p>
+                                        </div>
+                                    </div>
+                                @endif
+                                <table>
+                                    <tbody>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <tr>
+                                            <td class="r-o">Category:</td>
+                                            <td>{{ $phong->loaiphongs->ten }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="r-o">Capacity:</td>
+                                            <td>Max persion {{ $phong->loaiphongs->soluong }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
-                            @endif
-                            <table>
-                                <tbody>
-                                    <br>
-                                    <br>
-                                    <br>
-                                    <br>
-                                    <br>
-                                    <br>
-                                    <br>
-                                    <tr>
-                                        <td class="r-o">Category:</td>
-                                        <td>{{$phong->loaiphongs->ten}}</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="r-o">Capacity:</td>
-                                        <td>Max persion {{$phong->loaiphongs->soluong}}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
                         </div>
                     </div>
-                </div>
                 @endforeach
                 {{-- Roon load end --}}
             </div>
